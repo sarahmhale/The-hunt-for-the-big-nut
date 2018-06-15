3030ac86265b3f9cadf03b2e420d0975991143db
Sarah <sarahhale95@gmail.com>
2018-06-15 14:11:34 +0200

    setup movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public Rigidbody rigidbody;
+	public float jumpForce;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+		
+	// Update is called once per frame
+	void Update ()
+	{
+		rigidbody.velocity = new Vector3 (
+			Input.GetAxis ("Horizontal") * moveSpeed,
+			rigidbody.velocity.y,
+			Input.GetAxis ("Vertical") * moveSpeed);
+
+		if(Input.GetButtonDown("Jump")){
+			rigidbody.velocity = new Vector3 (
+				rigidbody.velocity.x,
+				jumpForce,
+				rigidbody.velocity.z);
+		}
+	}
+}
