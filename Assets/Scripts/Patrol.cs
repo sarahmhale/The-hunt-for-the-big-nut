9174dc8525ef631a974b22a2d76119941682d0ee
Sarah <sarahhale95@gmail.com>
2018-07-06 14:08:14 +0200

    enemy walks between points

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+	public Transform[] patrolPoints;
+	public float moveSpeed;
+
+	private int currentPointIndex = 0;
+	// Use this for initialization
+	void Start () {
+		transform.position = patrolPoints [0].position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position == patrolPoints [currentPointIndex].position) {
+			currentPointIndex++;
+			if (currentPointIndex == patrolPoints.Length) {
+				currentPointIndex = 0;
+			}
+		}
+
+		transform.position = Vector3.MoveTowards (
+			transform.position,
+			patrolPoints [currentPointIndex].position,
+			moveSpeed * Time.deltaTime
+		);
+		
+	}
+}
